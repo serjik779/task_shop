ad575f6987c7bff9e82564627026fa4c8e6f1d0b
iamsupergirl <alona.ant@bk.ru>
2017-08-17 00:41:06 +0300

    About and Feedback static pages

--- src/ShopBundle/Controller/DefaultController.php
+++ src/ShopBundle/Controller/DefaultController.php
@@ -2,7 +2,9 @@
 
 namespace ShopBundle\Controller;
 
+use ShopBundle\Entity\Feedback;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
@@ -11,10 +13,43 @@ class DefaultController extends Controller
         return $this->render('ShopBundle:home:index.html.twig');
     }
 
-    public function contactVendorAction()
+    public function contactVendorAction(Request $request)
     {
+        $feedback = new Feedback();
+
+        $form = $this->createFormBuilder($feedback)
+            ->add('name', \Symfony\Component\Form\Extension\Core\Type\TextType::class, array(
+                'label' => 'Name'))
+            ->add('email', EmailType::class, array(
+                'label' => 'Email'))
+            ->add('text', TextareaType::class, array(
+                'label' => 'Message'))
+            ->add('save', SubmitType::class, array(
+                'label' => 'Send'))
+            ->getForm();
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $feedback = $form->getData();
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($feedback);
+            $em->flush();
+
+            return $this->redirectToRoute('contact_vendor');
+        }
+
         return $this->render('ShopBundle:Default:contactVendor.html.twig', array(
             'navigation_active' => 'others',
+            'form' => $form->createView(),
+        ));
+    }
+
+    public function aboutAction()
+    {
+
+        return $this->render('ShopBundle:Default:about.html.twig', array(
+            'navigator_active' => 'others',
+
         ));
     }
 }
