c4326cd0adfe0f17111f11942bd1724cffacf727
Pavel <pasha.167988@yandex.by>
2017-08-17 19:08:02 +0300

    cart

---
+++ src/ShopBundle/Controller/CartController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace ShopBundle\Controller;
+
+use ShopBundle\Entity\cart;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class CartController extends Controller
+{
+
+    public function indexAction() {
+        return $this->render('ShopBundle:Cart:cart.html.twig');
+    }
+
+}
