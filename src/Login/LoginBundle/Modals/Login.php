0b19944465e03c241d78838ec060df76f3c615b4
Mikhail Sytyj <sytyj.mikhael@gmail.com>
2017-08-17 18:42:48 +0300

    login

---
+++ src/Login/LoginBundle/Modals/Login.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Login\LoginBundle\Modals;
+
+
+class Login{
+
+    private $email;
+    private $password;
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param mixed $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+
+
+}
