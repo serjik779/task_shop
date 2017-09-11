6e067a28ac3e43f0d3edfe3887fb24567d86d218
Pavel <pasha.167988@yandex.by>
2017-09-11 15:50:06 +0300

    orders v1

---
+++ src/ShopBundle/Controller/MyOrdersController.php
@@ -0,0 +1,22 @@
+<?php
+namespace ShopBundle\Controller;
+
+use ShopBundle\Entity\Products;
+use Application\Sonata\UserBundle\Entity\User;
+use ShopBundle\Entity\OrdersInfo;
+use ShopBundle\Entity\OrderItems;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
+class MyOrdersController extends Controller
+{
+    public function myordersAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $orders = $em->getRepository(OrdersInfo::class)->findBy(array ('id'=>$this->getUser()));
+
+        return $this->render('ShopBundle:MyOrders:myorders.html.twig', array(
+            'orders' => $orders
+        ));
+    }
+}
